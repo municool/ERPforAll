9a37443a93e60b51bd7565fbe15032d99f66e431
Louis <herren.louis@gmx.ch>
2022-09-26 16:22:24 +0200

    added sells and purchaes

---
+++ ERPforAll_Ui/ERPforAll/Server/Controllers/PurchaseController.cs
@@ -0,0 +1,77 @@
+﻿using ERPforAll.Server.Infrastructure;
+using ERPforAll.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERPforAll.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PurchaseController : ControllerBase
+    {
+        private readonly IDbContextFactory<ErpDBContext> _dbContextFactory;
+
+        public PurchaseController(IDbContextFactory<ErpDBContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        [HttpGet]
+        public IEnumerable<Purchase> GetAllPurchases()
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                return context.Purchases.Include("Vendor").Include("Item").ToList();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public Purchase? GetById(int id)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                return context.Purchases.Include("Vendor").Include("Item").FirstOrDefault(v => v.Id == id);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateItem([FromBody] Purchase purchase)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                purchase.VendorId = purchase.Vendor.Id;
+                purchase.Vendor = null;
+                purchase.ItemId = purchase.Item.Id;
+                purchase.Item = null;
+                purchase.Date = DateTime.Now;
+                purchase.TradeId = Guid.NewGuid();
+
+                context.Purchases.Add(purchase);
+                context.SaveChanges();
+            }
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult UpdateItem(int id, [FromBody] Purchase purchase)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                var oldPurchase = context.Purchases.Find(purchase.Id);
+
+                if (oldPurchase != null)
+                {
+                    purchase.VendorId = purchase.Vendor.Id;
+                    purchase.Vendor = null;
+                    oldPurchase.Item = purchase.Item;
+                    oldPurchase.ItemId = purchase.Item.Id;
+                    oldPurchase.Amount = purchase.Amount;
+
+                    context.Purchases.Update(oldPurchase);
+                    context.SaveChanges();
+                }
+            }
+            return Ok();
+        }
+    }
+}
---
+++ ERPforAll_Ui/ERPforAll/Server/Controllers/SellController.cs
@@ -0,0 +1,77 @@
+﻿using ERPforAll.Server.Infrastructure;
+using ERPforAll.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERPforAll.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SellController : ControllerBase
+    {
+        private readonly IDbContextFactory<ErpDBContext> _dbContextFactory;
+
+        public SellController(IDbContextFactory<ErpDBContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        [HttpGet]
+        public IEnumerable<Sell> GetAllSells()
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                return context.Sells.Include("Customer").Include("Item").ToList();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public Sell? GetById(int id)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                return context.Sells.Include("Customer").Include("Item").FirstOrDefault(v => v.Id == id);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateItem([FromBody] Sell sell)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                sell.CustomerId = sell.Customer.Id;
+                sell.Customer = null;
+                sell.ItemId = sell.Item.Id;
+                sell.Item = null;
+                sell.Date = DateTime.Now;
+                sell.TradeId = Guid.NewGuid();
+
+                context.Sells.Add(sell);
+                context.SaveChanges();
+            }
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult UpdateItem(int id, [FromBody] Sell sell)
+        {
+            using (var context = _dbContextFactory.CreateDbContext())
+            {
+                var oldSell = context.Sells.Find(sell.Id);
+
+                if (oldSell != null)
+                {
+                    sell.CustomerId = sell.Customer.Id;
+                    sell.Customer = null;
+                    oldSell.Item = sell.Item;
+                    oldSell.ItemId = sell.Item.Id;
+                    oldSell.Amount = sell.Amount;
+
+                    context.Sells.Update(oldSell);
+                    context.SaveChanges();
+                }
+            }
+            return Ok();
+        }
+    }
+}
